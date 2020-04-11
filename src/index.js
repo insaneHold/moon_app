2492707b75ee339ae15805ae6aac450a0d0b89ad
insaneHold <cabreraespinozadelosmonteros@outlook.es>
2020-04-11 17:22:19 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const http = require('./app')
 require('./connection');
 
 async function init() {
-    await http.listen(process.env.PORT);
-    console.log('Server on localhost')
+    await http.listen(process.env.PORT || 3000 );
+    console.log('Server on localhost:3000')
 }
+
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
+  });
+
 
 init();
