35d319e0586fba977f211eb253139e711f3b3092
insaneHold <cabreraespinozadelosmonteros@outlook.es>
2020-04-11 17:41:21 -0500

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -8,8 +8,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(require('./controllers/authController'))
 
 
-const socketio = require('socket.io')(http)
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
 
+const socketio = require('socket.io')(http)
 socketio.on("connection", (userSocket) => {
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
